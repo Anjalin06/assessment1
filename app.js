4eefd71c15f9fa8fe3090392f37b9b122ccea8d6
Anjali N Menon <69088816+Anjalin06@users.noreply.github.com>
2020-09-25 06:35:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,54 @@
 var express=require('express');
 var app=express();
-var listofhotels=require('./hotel.json');
+var hotelList = require('./hotels.json');
 
 app.listen('3000',function(req,res){
 console.log("listening to port 3000");
 
 })
 
-app.get('/home',function(req,res){
-res.send('welcome to express');
-})
-
-
-app.get('/listofhotels',function(req,res){
-   res.send(listofhotels);
+app.get('/showhotel',function(req,res){
+   res.send(hotelList);
    })
 
-app.get('/hotelbycity',function(req,res){
-   var newhotel=[];
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   city1=req.query.city;
+app.get('/showcity',function(req,res){
+   var sublist=[];
+   city=req.query.city;
    var flag=0;
-   for(const ele in listofhotels)
+   for(const elem in hotelList)
    {
-   if(listofhotels[ele].city==city1){
-      newhotel.push(listofhotels[ele]);
+   if(hotelList[elem].city==city){
+      sublist.push(hotelList[elem]);
       flag=1;
    }
    }
    if(flag==0)
-   res.send("not found");
-   res.send(newhotel)
+   res.send("city not found");
+   res.send(sublist)
    
-   })
-   app.get('/hotelbytype',function(req,res){
-      var newhotel=[];
-      type=req.query.type;
-      var flag=0;
-      for(const ele in listofhotels)
-      {
-      if(listofhotels[ele].type==type){
-         newhotel.push(listofhotels[ele]);
-         flag=1;
-      }
-      }
-      if(flag==0)
-      res.send("not found");
-      res.send(newhotel)
-      
-      })
+})
+app.get('/showtype',function(req,res){
+    var type = req.query.type;
+    var sublist =[];
+    var flag =0;
+    for(const i in hotelList){
+        if(hotelList[i].type == type){
+            flag = 1;
+            sublist.push(hotelList[i]);
+            
+        }
+    }
+    
+    if(flag == 0)
+        res.send('type not found');
+        res.send(sublist);
+})
 
-     
-
-app.get("/hotelbycuisine", function(req, res)
+app.get("/showcuisine", function(req, res)
 {
     cuisine = req.query.cuisine;
     searchcuisine = [];
-    listofhotels.forEach(element =>
+    hotelList.forEach(element =>
     {
         for (const key in element.cuisine)
         {
